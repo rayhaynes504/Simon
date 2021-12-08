2b6d3a0bdaf59a4dee97d48c7c6d9966a9caa126
rayhaynes504 <twotone504@gmail.com>
2021-12-08 11:33:21 -0600

    add highlight on box click

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function getRandomNumber(min, max) {
 }
 
 /*----- app's state (variables) -----*/
- let compSequence = []
- let playerSequence = []
+let compSequence = [];
+let playerSequence = [];
 /*----- cached element references -----*/
 const flexboxContainerEl = document.querySelector('.flexbox-container');
 
@@ -31,36 +31,52 @@ const aboutEl = document.querySelector('.about');
 // strtBtn.addEventListener('click', )
 // aboutEl.addEventListener('click', )
 /*----- functions -----*/
-// Need fucntion to push random number into an array after each correct sequence response. 
+// Need fucntion to push random number into an array after each correct sequence response.
 //Need a way to initialize the game
 //Need a way to check if user input is the same as the array
 // Declare a winner.
-    function checkForMatch {
-        if (compSequence === playerSequence) {
-            congrats and continue prompt;
-        if (player continues) {
-            empty playerSequence Array;
-            push getRandomNumber(1, 4) into compSequence;
-        display compSequence;
-    receive playerSequence}
-        }
-    } else {
-        return try again prompt
-    }
+// function checkForMatch {
+//     if (compSequence === playerSequence) {
+//         congrats and continue prompt;
+//     if (player continues) {
+//         empty playerSequence Array;
+//         push getRandomNumber(1, 4) into compSequence;
+//     display compSequence;
+// receive playerSequence}
+//     }
+// } else {
+//     return try again prompt
+// }
 // Game flow
-    // click handler to start game
-    // make compSequence;
-    // display compSequence;
-    // receive playerSequence;
-    // compare sequences;
-    // if (sequencesmatch) {
-    //     congrats, continue? prompt;
-    //     if (continue === 'yes') {
-    //     empty player sequence;
-    //     add another element in compSequence;
-    //     } else {
-    //         return alert thanks for playing
-    //     }
-    // } else {
-    //     alert you guessed incorrectly. Thanks for playing.
-    // }
+// click handler to start game
+// make compSequence;
+// display compSequence;
+// receive playerSequence;
+// compare sequences;
+// if (sequencesmatch) {
+//     congrats, continue? prompt;
+//     if (continue === 'yes') {
+//     empty player sequence;
+//     add another element in compSequence;
+//     } else {
+//         return alert thanks for playing
+//     }
+// } else {
+//     alert you guessed incorrectly. Thanks for playing.
+// }
+
+// box1El.addEventListener('click', function (event) {
+// 	box1El.style.filter = 'brightness(200%)';
+// 	setTimeout(function () {
+// 		box1El.style.filter = 'brightness(100%)';
+// 	}, 350);
+// });
+
+function clickHighlight(event) {
+	event.target.style.filter = 'brightness(200%)';
+	setTimeout(function () {
+		event.target.style.filter = 'brightness(100%)';
+	}, 350);
+}
+
+flexboxContainerEl.addEventListener('click', clickHighlight);
